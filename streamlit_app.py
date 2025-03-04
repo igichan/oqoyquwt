56a2d5f7d6152d449c79fd2c52215e960e879d40
igichan <igichan52@gmail.com>
2025-03-04 13:29:56 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,11 @@
 import streamlit as st
-def main():
 
-password =st.text_input('비밀번호 입력', type='password')
+def main():
+    password = st.text_input('비밀번호 입력', type='password')
 
     if password:
         st.secrets["user_password"] = password
         st.success("비밀번호가 안전하게 저장되었습니다.")
-        
+
 if __name__ == '__main__':
     main()
